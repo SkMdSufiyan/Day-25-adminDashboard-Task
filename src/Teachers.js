955a94098210e8c9c7fbfee7f61f681efcac92b1
SkMdSufiyan <107339274+SkMdSufiyan@users.noreply.github.com>
2023-01-22 08:26:55 +0530

    Update Teachers.js

--- src/Teachers.js
+++ src/Teachers.js
@@ -13,6 +13,9 @@ function Teachers(){
     const handleDeleteTeacher = context.handleDeleteTeacher;
 
     return <Container className="main-container">
+        
+    <p className="netlify-suggestion-p">Kindly <b>DO NOT REFRESH</b> this netlify site when you are in the page other than home (dashboard) page. If you refresh on page other than home page, then it will give "page not found" (problem with netlify deployment). </p>
+    
     <br />
     <h5>Teachers-list page</h5>
     <br />
